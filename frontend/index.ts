7d67e04a50a48987674d4cf67ad200923f9fd5e5
Mykola Cherednychenko <niki.chered@gmail.com>
2021-09-02 21:13:09 +0200

    Second commit

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,13 @@
 import { Router } from '@vaadin/router';
 import { routes } from './routes';
 import { appStore } from './stores/app-store';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-grid';
+import '@vaadin/vaadin-text-field';
+
+window.addEventListener('load', () => {
+  initUI();
+});
 
 export const router = new Router(document.querySelector('#outlet'));
 
@@ -15,3 +22,24 @@ window.addEventListener('vaadin-router-location-changed', (e) => {
     document.title = appStore.applicationName;
   }
 });
+
+function initUI() {
+  const firstNameField = document.querySelector('#firstName');
+  const lastNameField = document.querySelector('#lastName');
+  const addButton = document.querySelector('#addButton');
+  const grid = document.querySelector('#grid');
+  let people = [];
+
+  addButton.addEventListener('click', e => {
+    people = [
+      ...people,
+      {
+        firstName: firstNameField.value,
+        lastName: lastNameField.value
+      }
+    ];
+    grid.items = people;
+    firstNameField.value = '';
+    lastNameField.value = '';
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,28 @@
 const config = require("./webpack.generated.js");
 module.exports = config;
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = ({ mode }) => {
+  return {
+    mode,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new CopyWebpackPlugin([
+        {
+          context: 'node_modules/@webcomponents/webcomponentsjs',
+          from: '**/*.js',
+          to: 'webcomponents'
+        }
+      ])
+    ],
+    devtool: mode === 'development' ? 'source-map' : 'none'
+  };
+};
+
 //const merge = require('webpack-merge');
 //const flowDefaults = require('./webpack.generated.js');
 
